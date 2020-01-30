c71c540722746cce322c232d6bf2a00a8b8f951d
SemiColle <sephiroth@gmx.at>
2020-01-30 23:02:01 +0100

    Added some debuff icons for E3S. Changed ranged dps to archer

--- SemiClear/AssetPath.py
+++ SemiClear/AssetPath.py
@@ -6,3 +6,10 @@ RANGED = r'assets\FFXIVIcons JobIcons\00_ROLE\RangedRole.png'
 TITANIA = r'assets\Bosses\Titania_small.png'
 
 HITBOX = r'assets\Icons\hitbox.png'
+
+SMOTHERING_TSUNAMI = r'assets\De-Buffs\smothering_tsunami.png'
+SUNDERING_TSUNAMI = r'assets\De-Buffs\sundering_tsunami.png'
+SURGING_TSUNAMI = r'assets\De-Buffs\surging_tsunami.png'
+SWEEPING_TSUNAMI = r'assets\De-Buffs\sweeping_tsunami.png'
+SPLASHING_WATERS = r'assets\De-Buffs\splashing_waters.png'
+SWIRLING_WATERS = r'assets\De-Buffs\swirling_waters.png'
